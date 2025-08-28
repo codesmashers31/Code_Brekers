3d86e3b42ebb6a8bcdea30380dc20292c8f6bbe1
balasudhan <balasudhan17@gmail.com>
2025-08-28 18:40:43 +0530

    Udpated

---
+++ JS-Language/JS/intro.js
@@ -0,0 +1,76 @@
+// var a = 10;
+// var a = 12;
+
+// var a;
+
+
+// let a = 10;
+// a = 12;
+
+// let a = 10;
+
+// let a;
+// let a;
+
+
+
+// let a = "This is js";
+
+// console.log(a);
+
+// alert(a);
+
+// var a;
+
+// console.log(a);
+
+
+
+// var b = "new ai"
+// var a = 12;
+// // let b = false;
+
+// // let c  = a + b;
+
+
+
+// a = null;
+
+
+// var a = "Hello"
+// console.log(Number(a))
+
+
+// let a = [1,2,3,4,5,"this is array"];
+
+// console.log(a[5]);
+
+// let obj = [{name:"react",age:12,address:"Chennai"},{name:"REact 1",age:18,address:"Ramapuarm"}]
+
+// console.log(obj.name);
+
+
+
+ function add(){
+
+    console.log("This is Normal Function When you call the function where ever you want. its is running");
+    
+
+}
+
+
+const expression = function(){
+    console.log("Anonimus & Function express , When you call the function where ever you want. its is running");
+}
+
+
+
+const newvalue = () =>
+    {
+        console.log("arraow function ,When you call the function where ever you want. its is running");
+    }
+
+add()
+
+expression()
+newvalue()
